4f92622097a4eca8cdae84616bbe6b066de9797c
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2023-03-18 18:28:18 +0800

    update alpinejs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,11 @@
 <x-guest-layout>
-    <div class="">
-        <p class="text-white text-center">Welcome to Generic National School</p>
+    @include('home.nav')
+    <div class="row h-[100vh] w-[100%] bg-fixed" style="background-image: url('/img/bg-hero.jpg')">
+        <div class="">
+            <p class="text-black text-center">Welcome to Generic National School</p>
+        </div>
+    </div>
+    <div class="row h-[100vh] w-[100%] bg-[#4F6D7A]">
+        <p class="">about</p>
     </div>
 </x-guest-layout>
